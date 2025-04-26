86f14a21e31363f0790beba4eaec115aa7458e69
sazh0 <saraabdulazizzh@gmail.com>
2025-04-27 00:34:54 +0300

    Add files via upload

---
+++ build/precache-manifest.fa44a63ef9b6af99963246c8d25530ba.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f2d7cc01c4ed74ce3e6bece4738b7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce8a56859cb18c9b708",
+    "url": "/static/css/main.373ff32c.chunk.css"
+  },
+  {
+    "revision": "38ada4a75ff64a66380f",
+    "url": "/static/js/2.5c0ecff7.chunk.js"
+  },
+  {
+    "revision": "5e13618ed677c6056cc34ac473016e81",
+    "url": "/static/js/2.5c0ecff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce8a56859cb18c9b708",
+    "url": "/static/js/main.26e5966f.chunk.js"
+  },
+  {
+    "revision": "af7c03e2b11f6a567146",
+    "url": "/static/js/runtime-main.eb11466f.js"
+  },
+  {
+    "revision": "a7bb93fd5a0ffd23ce7388170f151030",
+    "url": "/static/media/2030.a7bb93fd.svg"
+  },
+  {
+    "revision": "09065a147c037d86ed3919d684b60088",
+    "url": "/static/media/AWJLOGO.09065a14.svg"
+  },
+  {
+    "revision": "f2266e9ee20d7b0b52b13c6086af032a",
+    "url": "/static/media/AWJwithName.f2266e9e.svg"
+  },
+  {
+    "revision": "eac803d4781f29bb105d7d9212b65f43",
+    "url": "/static/media/Aws.eac803d4.png"
+  },
+  {
+    "revision": "f32ddb959f20f6d0130cd18ae12d99bb",
+    "url": "/static/media/BMICON.f32ddb95.svg"
+  },
+  {
+    "revision": "50ce842614e6d7c6b115690986950648",
+    "url": "/static/media/BMIconD.50ce8426.svg"
+  },
+  {
+    "revision": "f96af89d108a5248bbed465bad260f1a",
+    "url": "/static/media/BMLOGO.f96af89d.svg"
+  },
+  {
+    "revision": "850af386af46f59826c8ae98de45a75d",
+    "url": "/static/media/BlueBox.850af386.svg"
+  },
+  {
+    "revision": "fdc57048ec2666cbe880dec57aa1e1ed",
+    "url": "/static/media/CompanyFileIcon.fdc57048.svg"
+  },
+  {
+    "revision": "622627121c29788314dda324c8a6a8c3",
+    "url": "/static/media/DashBIcon.62262712.svg"
+  },
+  {
+    "revision": "570d3bef0ad08dd20247fe87cb2750bc",
+    "url": "/static/media/GDIcon.570d3bef.svg"
+  },
+  {
+    "revision": "77e4770fe6692f3b3e65d4b80b9efc42",
+    "url": "/static/media/GD_LOGO.77e4770f.svg"
+  },
+  {
+    "revision": "baa55116c7cb449fd23df7b33ee832b2",
+    "url": "/static/media/GoalDecomposeIcon.baa55116.svg"
+  },
+  {
+    "revision": "29eb7ade2d7cdffa1a814af8c71113e6",
+    "url": "/static/media/Intersect.29eb7ade.png"
+  },
+  {
+    "revision": "794db9491de889f950b660492c4046cf",
+    "url": "/static/media/LeftArrow.794db949.svg"
+  },
+  {
+    "revision": "acc3eea51615c134518f18d5f8216041",
+    "url": "/static/media/MONSHAAT.acc3eea5.png"
+  },
+  {
+    "revision": "ff90987c25e89774807d95193af0dcf9",
+    "url": "/static/media/ProjIcon.ff90987c.svg"
+  },
+  {
+    "revision": "697050aa0d63c290eab26010fdcea5af",
+    "url": "/static/media/Rectangle102.697050aa.svg"
+  },
+  {
+    "revision": "41b51e591ca166242c38923e20258b91",
+    "url": "/static/media/Rectangle103.41b51e59.svg"
+  },
+  {
+    "revision": "e12e74b7c3733c391d3ce768d21a0b75",
+    "url": "/static/media/RedBlueBox.e12e74b7.svg"
+  },
+  {
+    "revision": "9aba81e23b36f54d9bfb832fd7dea6ca",
+    "url": "/static/media/SideSqrs.9aba81e2.svg"
+  },
+  {
+    "revision": "8868dfaff3a53006a7ebbe8ecd3df28b",
+    "url": "/static/media/SmallSqrs1.8868dfaf.svg"
+  },
+  {
+    "revision": "b2be5bf5fb70e4195e382ce6eb8de13c",
+    "url": "/static/media/SmallSqrs2.b2be5bf5.svg"
+  },
+  {
+    "revision": "e0c0eb7558a00bd7281361f7dd2a2b1c",
+    "url": "/static/media/directions.e0c0eb75.png"
+  },
+  {
+    "revision": "0114349f210d81e2e98cd5da9d8380cd",
+    "url": "/static/media/easy-to-use.0114349f.png"
+  },
+  {
+    "revision": "36e0c02dbba874cdfc662d638953a868",
+    "url": "/static/media/logo.36e0c02d.png"
+  },
+  {
+    "revision": "5c80ee1b4f74d0780e0def3f86814299",
+    "url": "/static/media/shuttle.5c80ee1b.png"
+  },
+  {
+    "revision": "de13f015ea1a2729c7cc2a5284ec1178",
+    "url": "/static/media/sqrs.de13f015.svg"
+  }
+]);
